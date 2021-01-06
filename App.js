d13b6ed455728f7700d2ea750707ee8b9da47f3f
unknown <onurcoskun99@gmail.com>
2021-01-06 15:28:34 +0300

    header section

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import io from 'socket.io-client'
 
 
 
 
 export default function App() {
-  const [userData, setUserData] = useState({ name: 'madonna' })
-  const mainRef = useRef(null);
+  const [userDataList, setUserDataList] = useState([])
 
-  useEffect(() => {
-    const socket = io('wss://wunder-provider.herokuapp.com/')
-    socket.on('userList', (stm) => {
-      console.log(stm.results[0].name.first)
-    })
-  })
+
+  // useEffect(() => {
+  //   const socket = io('wss://wunder-provider.herokuapp.com/')
+  //   socket.on('userList', (stm) => {
+  //     console.log(stm.results[0].name.first)
+  //   })
+  // })
 
 
   return (
-    <View style={styles.container} ref={mainRef}>
+    <View style={styles.container} >
+      <View style={styles.head}>
+        <Text style={styles.header}>PROFILES</Text>
+        <View>
+          <View style={styles.lineFirst}></View>
+          <View style={styles.lineRest}></View>
+          <View style={styles.lineRest}></View>
+        </View>
 
+      </View>
       <View>
         <Text>hello</Text>
       </View>
+
 
       <StatusBar style="auto" />
 
@@ -35,7 +44,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
+  head: {
+    width: '100%',
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  header: {
+    width: 103,
+    height: 25,
+    fontSize: 20,
+    textAlign: 'right',
+    color: '#b1b8c5',
+  },
+  lineFirst: {
+    width: 32,
+    height: 1,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: '#6374f3',
+    borderRadius: 4,
+  },
+  lineRest: {
+    width: 32,
+    height: 1,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: '#6374f3',
+    borderRadius: 4,
+    marginTop: 9,
+  }
 });
