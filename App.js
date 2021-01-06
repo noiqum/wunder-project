1472986feb529d5ba4ebec91ee2afe2ad87871b1
unknown <onurcoskun99@gmail.com>
2021-01-06 13:31:43 +0300

    websocket connection

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import io from 'socket.io-client'
+
+
+
 
 export default function App() {
+  const [userData, setUserData] = useState({ name: 'madonna' })
+  const mainRef = useRef(null);
+
+  useEffect(() => {
+    const socket = io('wss://wunder-provider.herokuapp.com/')
+    socket.on('userList', (stm) => {
+      console.log(stm.results[0].name.first)
+    })
+  })
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} ref={mainRef}>
+
+      <View>
+        <Text>hello</Text>
+      </View>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
